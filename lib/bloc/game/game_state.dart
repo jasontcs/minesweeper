f3af58537161182bb32e91d811de174cd9516e40
jasontcs <10681380+jasontcs@users.noreply.github.com>
2022-02-09 20:28:27 +0800

    remove git ignore files

--- lib/bloc/game/game_state.dart
+++
@@ -1,9 +0,0 @@
-abstract class GameState {}
-
-class GameInitial extends GameState {}
-
-class GamePlaying extends GameState {}
-
-class GameSuccess extends GameState {}
-
-class GameFailure extends GameState {}
--- lib/main.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:minesweeper/util/singleton.dart';
-
-import 'app.dart';
-
-Future<void> main() async {
-  await AppSingleton.setup();
-  runApp(const App());
-}
