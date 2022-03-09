7d191a65f23f1860fff3a02c3134cbfc66799058
jasontcs <10681380+jasontcs@users.noreply.github.com>
2022-03-10 00:44:22 +0800

    add log

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,12 @@ import 'dart:html';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:minesweeper/util/singleton.dart';
 
 import 'app.dart';
 import 'firebase_options.dart';
+import 'util/bloc_observer.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -17,5 +19,10 @@ Future<void> main() async {
 
   if (kIsWeb) window.document.onContextMenu.listen((e) => e.preventDefault());
 
-  runApp(const App());
+  BlocOverrides.runZoned(
+    () {
+      runApp(const App());
+    },
+    blocObserver: AppBlocObserver(),
+  );
 }
---
+++ lib/util/bloc_observer.dart
@@ -0,0 +1,42 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:logger/logger.dart';
+
+class AppBlocObserver extends BlocObserver {
+  final Logger _logger = Logger();
+
+  @override
+  void onCreate(BlocBase bloc) {
+    super.onCreate(bloc);
+    _logger.d('[Bloc] onCreate -- ${bloc.runtimeType}');
+  }
+
+  @override
+  void onEvent(Bloc bloc, Object? event) {
+    super.onEvent(bloc, event);
+    _logger.d('[Bloc] onEvent -- ${bloc.runtimeType}, $event');
+  }
+
+  @override
+  void onChange(BlocBase bloc, Change change) {
+    super.onChange(bloc, change);
+    _logger.d('[Bloc] onChange -- ${bloc.runtimeType}, $change');
+  }
+
+  @override
+  void onTransition(Bloc bloc, Transition transition) {
+    super.onTransition(bloc, transition);
+    _logger.d('[Bloc] onTransition -- ${bloc.runtimeType}, $transition');
+  }
+
+  @override
+  void onError(BlocBase bloc, Object error, StackTrace stackTrace) {
+    _logger.d('[Bloc] onError -- ${bloc.runtimeType}, $error');
+    super.onError(bloc, error, stackTrace);
+  }
+
+  @override
+  void onClose(BlocBase bloc) {
+    super.onClose(bloc);
+    _logger.d('[Bloc] onClose -- ${bloc.runtimeType}');
+  }
+}
