0729237b3fd8f43aec471f28efd37f38b286841c
jasontcs <10681380+jasontcs@users.noreply.github.com>
2022-03-11 11:11:05 +0800

    No custom win record

--- lib/screen/widget/mine_game_grid.dart
+++ lib/screen/widget/mine_game_grid.dart
@@ -3,6 +3,7 @@ import 'package:minesweeper/bloc/difficulty/difficulty_bloc.dart';
 import 'package:minesweeper/bloc/game/game_bloc.dart';
 import 'package:minesweeper/bloc/timer/timer_bloc.dart';
 import 'package:minesweeper/bloc/win_record/win_record_bloc.dart';
+import 'package:minesweeper/model/difficulty_model.dart';
 import 'package:minesweeper/util/dialog.dart';
 
 import 'minebox.dart';
@@ -16,7 +17,9 @@ class MineGameGrid extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocConsumer<GameBloc, GameState>(
       listener: (context, state) async {
-        if (state is GameWin) {
+        if (state is GameWin &&
+            context.read<DifficultyBloc>().state.difficulty.option !=
+                DifficultyOption.custom) {
           String name = await AppPopUp.askName(context);
           DifficultyBloc difficultyBloc =
               BlocProvider.of<DifficultyBloc>(context);
